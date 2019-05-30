bc8f786fa07421b56667be4801c05fdf4bac40e2
Ivan Miletic <milemik68@gmail.com>
2019-05-30 02:03:42 +0200

    Added Scraper app and result page in app

--- scraper/urls.py
+++ scraper/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [,
+urlpatterns = [
     path('', views.scraper, name="scraper.html"),
+    path('results/', views.scraping_results, name="scraping_results.html")
 ]
--- scraper/views.py
+++ scraper/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def scraper(request):
 	return render(request, 'scraper.html')
+
+def scraping_results(request):
+	return render(request, 'scraping_results.html')
